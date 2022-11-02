9da6ac8c27c5d939e2fe41ab9aa6269b734b67fa
toan1231 <chauthai2003@gmail.com>
2022-11-01 23:47:04 -0400

    kdfjkdjf

--- src/test/java/pangiamHomePage/PangiamHomePageTest.java
+++ src/test/java/pangiamHomePage/PangiamHomePageTest.java
@@ -1,9 +1,13 @@
 package pangiamHomePage;
 
+<<<<<<< HEAD
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+=======
+import org.openqa.selenium.*;
+>>>>>>> 47610e8 (kdfjkdjf)
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -26,6 +30,11 @@ public class PangiamHomePageTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 driver.get("https://www.amazon.com/");
+<<<<<<< HEAD
+=======
+//hellow
+
+>>>>>>> 47610e8 (kdfjkdjf)
 
     }
 }
