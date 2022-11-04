ec327c6c1c2ccae7da222ec45bfb26ddfa5b2af6
toan1231 <chauthai2003@gmail.com>
2022-11-04 17:43:44 -0400

    new com

--- src/test/java/pangiamHomePage/PangiamHomePage.java
+++ src/test/java/pangiamHomePage/PangiamHomePage.java
@@ -1,9 +1,6 @@
 package pangiamHomePage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-//hhhhhhhhhhhhhhhhhhh
+this is 11
 
 
 }
