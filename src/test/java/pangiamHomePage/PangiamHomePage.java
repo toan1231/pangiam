92ab6b6241de95cd0661131177ef36261d43672c
toan1231 <chauthai2003@gmail.com>
2022-11-04 17:41:24 -0400

    new com

--- src/test/java/pangiamHomePage/PangiamHomePage.java
+++ src/test/java/pangiamHomePage/PangiamHomePage.java
@@ -3,12 +3,7 @@ package pangiamHomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-public class PangiamHomePage {
-    WebDriver driver;
-    public PangiamHomePage(WebDriver driver){
-        PageFactory.initElements(driver,this);
-    }
+//hhhhhhhhhhhhhhhhhhh
 
 
 }
